69deff329a846aaba2e1eaea53763955fda4f93d
Matamando Kalilani <matamandokalilani@outlook.com>
2022-07-06 15:34:16 +0200

    Starting... Orders

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,7 @@ import { PaymentCreatedListener } from "./events/listeners/payment-created-liste
 const port = 3000;
 
 const start = async () => {
+  console.log("Starting...");
   if (!process.env.JWT_KEY) {
     throw Error("JWT_KEY not defined");
   }
