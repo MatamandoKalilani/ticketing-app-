4ac3750dafe27ee5260ac489ed890ea8137bc5c5
Matamando Kalilani <matamandokalilani@outlook.com>
2022-07-06 18:43:44 +0200

    Save

--- client/api/build-client.ts
+++ client/api/build-client.ts
@@ -2,10 +2,12 @@ import axios, { AxiosRequestHeaders } from "axios";
 import { NextPageContext } from "next";
 import { isServer } from "../tools/client-or-server";
 
+//http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+
 const buildClient = ({ req }: NextPageContext) => {
   return axios.create({
     baseURL: isServer()
-      ? "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local"
+      ? "https://www.builtdifferent.dev/"
       : "/",
     headers: isServer() && req ? (req.headers as AxiosRequestHeaders) : {},
   });
